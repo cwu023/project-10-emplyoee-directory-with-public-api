a18e9dccd63db18a5f0d623846b0d9f51b7027f7
Jimmy <cwu023@gmail.com>
2018-08-01 14:28:47 -0700

    Added modal HTML
    Added modal HTML and click event on each employee card to show more data from API.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+//AJAX Request
 $.ajax({
-  url: 'https://randomuser.me/api/?results=12',
+  url: 'https://randomuser.me/api/?results=12&nat=us',
   dataType: 'json',
   success: function(data) {
+    //Employee cards HTML
     let employeeHTML = '';
     $.each(data.results, function(key, employee) {
         employeeHTML += '<div class="employee">';
@@ -12,5 +14,41 @@ $.ajax({
         employeeHTML += '<p class="city"> '+ employee.location.city +' </p></div></div>';
     });
     $('.wrapper').html(employeeHTML);
-  } //end success
-}); //end ajax
+
+    //Modal HTML
+    $('.employee').on('click', function(){
+      let employeeHTML = '';
+      let currentIndex = $(this).index();
+      let modalPic = data.results[currentIndex].picture.large;
+      let modalFirstName = data.results[currentIndex].name.first;
+      let modalLastName = data.results[currentIndex].name.last;
+      let modalEmail = data.results[currentIndex].email;
+      let modalCity = data.results[currentIndex].location.city;
+      let modalPhone = data.results[currentIndex].cell;
+      let modalStreet = data.results[currentIndex].location.street;
+      let modalState = data.results[currentIndex].location.state;
+      let modalPostCode = data.results[currentIndex].location.postcode;
+      let modalBirthday = data.results[currentIndex].dob.date.substr(0, 10); //only return part of the string
+
+      employeeHTML += '<div class="modal-content">';
+      employeeHTML += '<span class="close">&times;</span>';
+      employeeHTML += '<img src=" '+ modalPic + ' ">';
+      employeeHTML += '<p class="name"> '+ modalFirstName +' '+ modalLastName +'</p>'
+      employeeHTML += '<p class="email"> '+ modalEmail +' </p>';
+      employeeHTML += '<p class="city"> '+ modalCity +' </p>';
+      employeeHTML += '<hr>';
+      employeeHTML += '<p class="phone"> '+ modalPhone +' </p>';
+      employeeHTML += '<p class="address"> '+ modalStreet +''+','+' '+ modalCity +''+','+' '+ modalState +' '+ modalPostCode +'</p>';
+      employeeHTML += '<p class="birthday"> '+' Birthday: '+' '+ modalBirthday +' </p></div></div>';
+      $('.modal').html(employeeHTML);
+
+      //Open modal
+      $('.modal').fadeIn();
+      //Close modal
+      $('.close').on('click', function(){
+        $('.modal').fadeOut();
+      });
+    });
+
+  } //End success
+}); //End AJAX
